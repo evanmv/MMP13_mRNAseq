76516c59935b46aaf9c5d7d460fb1ad9705f00f8
ec-evanmv <evanmv@uio.no>
2024-01-05 09:44:25 +0100

    Add batch scripts for HISAT2 and TrimGalore

---
+++ TrimGalore_script.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#SBATCH --job-name=TrimGalore
+#SBATCH --account=ec12
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=1G
+
+#setup
+set -o errexit
+set -o nounset
+
+module --quiet purge
+
+module load TrimGalore/0.6.10-GCCcore-11.3.0
+
+#Argument
+fq1=$1
+fq2=$2
+
+#Run TrimGalore
+trim_galore --paired $SUBMITDIR/Raw_reads/$fq1 $SUBMITDIR/Raw_reads/$fq2 
