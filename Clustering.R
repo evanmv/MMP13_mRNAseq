1d53c418d611f936d2175dd11da43be9fffdcc26
evanmv <evanmv@uio.no>
2024-03-19 10:21:32 +0100

    Reran data analysis without independent filtering

--- Clustering.R
+++ Clustering.R
@@ -10,39 +10,42 @@ library(org.Hs.eg.db)
 
 ## Data -----
 
-vsd.hmap <- assay(vsd) %>%
+vsd.nf.hmap <- assay(vsd.nf) %>%
   as.data.frame()
-geneSymbols <- getSYMBOL(rownames(vsd.hmap), data = 'org.Hs.eg.db')
+geneSymbols <- getSYMBOL(rownames(vsd.nf.hmap), data = 'org.Hs.eg.db')
 
-vsd.hmap <- vsd.hmap %>%
+vsd.nf.hmap <- vsd.nf.hmap %>%
   mutate(Symbol = geneSymbols, .before = 1)
 
-vsd.hmap <- na.omit(vsd.hmap) #Elimate NAs from missing gene symbols
+vsd.nf.hmap <- na.omit(vsd.nf.hmap) #Elimate NAs from missing gene symbols
 
-vsd.hmap.sig <- vsd.hmap[rownames(vsd.hmap) %in% DEG2,]
+DEG.nf <- as.vector(resSig.nf@rownames)
+vsd.nf.hmap.sig <- vsd.nf.hmap[rownames(vsd.nf.hmap) %in% DEG.nf,]
 #Assign row names from 1st column containing geneSymbols and remove column
-row.names(vsd.hmap.sig) <- vsd.hmap.sig[,1] 
-vsd.hmap.sig[,1] <- NULL
-
-head(vsd.hmap.sig)
+row.names(vsd.nf.hmap.sig) <- vsd.nf.hmap.sig[,1] 
+vsd.nf.hmap.sig[,1] <- NULL
 
 #Narrow down DEG list to ca. 20
 
-resSigNarrow <- subset(resSig, log2FoldChange <= -0.8 | log2FoldChange >= 0.8) 
-resSigTidy <- as_tibble(resSigNarrow) %>%
-  mutate(GeneID = resSigNarrow@rownames, .before = 1)
+resSig.nf.Narrow <- subset(resSig.nf, log2FoldChange <= -1 | log2FoldChange >= 1)
+resSig.nf.Narrow <- as_tibble(resSig.nf.Narrow) %>%
+  mutate(GeneID = resSig.nf.Narrow@rownames, .before = 1)
 
-vsd.hmap.sigNarrow <- vsd.hmap[rownames(vsd.hmap) %in% resSigTidy$GeneID,]
+#resSigNarrow <- subset(resSig, log2FoldChange <= -0.8 | log2FoldChange >= 0.8) 
+#resSigTidy <- as_tibble(resSigNarrow) %>%
+ # mutate(GeneID = resSigNarrow@rownames, .before = 1)
 
-row.names(vsd.hmap.sigNarrow) <- vsd.hmap.sigNarrow[,1] 
-vsd.hmap.sigNarrow[,1] <- NULL
+vsd.nf.hmap.sigNarrow <- vsd.nf.hmap[rownames(vsd.nf.hmap) %in% resSig.nf.Narrow$GeneID,]
+
+row.names(vsd.nf.hmap.sigNarrow) <- vsd.nf.hmap.sigNarrow[,1] 
+vsd.nf.hmap.sigNarrow[,1] <- NULL
   
 ?subset
 
 ## Cluster DEGs ----
-
-clustRows <- hclust(as.dist(1-cor(t(vsd.hmap.sig), method="pearson")), method="complete") 
-clustColumns <- hclust(as.dist(1-cor(vsd.hmap.sig, method="spearman")), method="complete") #cluster columns by spearman correlation
+#Narrowed non-filtered DEGs (FC 1, padj 0.05)
+clustRows <- hclust(as.dist(1-cor(t(vsd.nf.hmap.sigNarrow), method="pearson")), method="complete") 
+clustColumns <- hclust(as.dist(1-cor(vsd.nf.hmap.sigNarrow, method="spearman")), method="complete") #cluster columns by spearman correlation
 module.assign <- cutree(clustRows, k=2)
 module.color <- rainbow(length(unique(module.assign)), start=0.1, end=0.9) 
 module.color <- module.color[as.vector(module.assign)] 
@@ -59,14 +62,14 @@ module.colorN <- module.colorN[as.vector(module.assignN)]
 ## Heatmap -----
 
 myheatcolors <- rev(brewer.pal(name="RdBu", n=11))
-
-heatmap.2(as.matrix(vsd.hmap.sig), 
+#Narrow, non-filtered...
+heatmap.2(as.matrix(vsd.nf.hmap.sigNarrow), 
           Rowv=as.dendrogram(clustRows), 
           Colv=as.dendrogram(clustColumns),
           RowSideColors=module.color,
-          col=rev(myheatcolors), scale='row', labRow=NA,
+          col=rev(myheatcolors), scale='row',
           density.info="none", trace="none",  
-          cexRow=1, cexCol=2, margins = c(4,4),
+          cexRow=1, cexCol=2, margins = c(4,6),
           key = TRUE, keysize = 2,
 )
 
--- volcanoPlot.R
+++ volcanoPlot.R
@@ -5,41 +5,42 @@ library(org.Hs.eg.db)
 library(plotly)
 
 ##Data -----
-resSymbols <- getSYMBOL(res.vplot$GeneID, data = 'org.Hs.eg.db')
-res.vplot <- as_tibble(resOrdered) %>%
-  mutate(GeneID = resOrdered@rownames, .before = 1) %>%
-  mutate(Symbol = resSymbols, .before=1)
+res.nf.vplot <- as_tibble(res.nf) %>%
+  mutate(GeneID = res.nf@rownames, .before = 1) 
 
-#res.vplot <- na.omit(res.vplot) ? Get rid of independent filtering?
+res.nf.Symbols <- getSYMBOL(res.nf.vplot$GeneID, data = 'org.Hs.eg.db')
+
+res.nf.vplot <- mutate(res.nf.vplot, Symbol = res.nf.Symbols, .before=1)
 
 ##Volcano plot -----
 
-vplot <- ggplot(res.vplot) +
+vplot <- ggplot(res.nf.vplot) +
   aes(y=-log10(padj), x=log2FoldChange, text = paste("Symbol:", Symbol)) +
   geom_point(size=1) +
   geom_hline(yintercept = -log10(0.05), linetype="longdash", colour="grey", size=1) +
   geom_vline(xintercept = 0.8, linetype="longdash", colour="#2C467A", size=1) +
   geom_vline(xintercept = -0.8, linetype="longdash", colour="#BE684D", size=1) +
-  labs(title="Volcano plot",
-       subtitle = "MMP13 kd",
-       caption=paste0("produced on ", Sys.time())) +
-  theme_bw()
-
-ggplotly(vplot)
-
-## With p-value (not adjusted) to get an idea without all the NAs
-ggplot(res.vplot) +
-  aes(y=-log10(pvalue), x=log2FoldChange, text = paste("Symbol:", Symbol)) +
-  geom_point(size=1) +
-  geom_hline(yintercept = -log10(0.05), linetype="longdash", colour="grey", size=1) +
-  geom_vline(xintercept = 0.8, linetype="longdash", colour="#2C467A", size=1) +
-  geom_vline(xintercept = -0.8, linetype="longdash", colour="#BE684D", size=1) +
-  
   ylim(NA, 75) +
   labs(title="Volcano plot",
        subtitle = "MMP13 kd",
        caption=paste0("produced on ", Sys.time())) +
   theme_bw()
 
-ggsave("vplot.png")
+ggplotly(vplot)
+ggsave("vplotYlim.nf.png")
+## With p-value (not adjusted) to get an idea without all the NAs
+#ggplot(res.vplot) +
+#  aes(y=-log10(pvalue), x=log2FoldChange, text = paste("Symbol:", Symbol)) +
+ # geom_point(size=1) +
+  #geom_hline(yintercept = -log10(0.05), linetype="longdash", colour="grey", size=1) +
+  #geom_vline(xintercept = 0.8, linetype="longdash", colour="#2C467A", size=1) +
+  #geom_vline(xintercept = -0.8, linetype="longdash", colour="#BE684D", size=1) +
+  
+  #ylim(NA, 75) +
+  #labs(title="Volcano plot",
+    #   subtitle = "MMP13 kd",
+     #  caption=paste0("produced on ", Sys.time())) +
+  #theme_bw()
+
+#ggsave("vplot.png")
 ?ggplot
