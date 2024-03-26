099ea7d37ceb3bfe95c89c404663e553e6598099
evanmv <evanmv@uio.no>
2024-03-26 15:45:13 +0100

    Patchwork to combine vplots into figure

--- volcanoPlot.R
+++ volcanoPlot.R
@@ -3,7 +3,8 @@ library(tidyverse)
 library(annotate)
 library(org.Hs.eg.db)
 library(plotly)
-
+install.packages("patchwork")
+library(patchwork)
 ##Data -----
 res.nf.vplot <- as_tibble(res.nf) %>%
   mutate(GeneID = res.nf@rownames, .before = 1) 
@@ -23,7 +24,7 @@ vplot <- ggplot(res.nf.vplot) +
   ylim(NA, 75) +
   labs(title="Volcano plot",
        subtitle = "MMP13 kd",
-       caption=paste0("produced on ", Sys.time())) +
+      ) +
   theme_bw()
 
 ggplotly(vplot)
@@ -44,3 +45,38 @@ ggsave("vplotYlim.nf.png")
 
 #ggsave("vplot.png")
 ?ggplot
+
+#DOWN
+vplotdown <- ggplot(res.nf.vplot) +
+  aes(y=-log10(padj), x=log2FoldChange, text = paste("Symbol:", Symbol)) +
+  geom_point(size=1.5, colour = "red") +
+  geom_hline(yintercept = -log10(0.05), linetype="longdash", colour="grey", size=1) +
+  geom_vline(xintercept = -1.0, linetype="longdash", colour="#BE684D", size=1) +
+  ylim(1.30, 40) +
+  xlim(-2, -1) +
+  labs(title = "Downregulated genes") +
+  theme_bw()
+
+ggplotly(vplotdown)
+
+#UP
+vplotup <- ggplot(res.nf.vplot) +
+  aes(y=-log10(padj), x=log2FoldChange, text = paste("Symbol:", Symbol)) +
+  geom_point(size=1.5, colour = "blue") +
+  geom_hline(yintercept = -log10(0.05), linetype="longdash", colour="grey", size=1) +
+  geom_vline(xintercept = 1.0, linetype="longdash", colour="#2C467A", size=1) +
+  ylim(1.30, 5) +
+  xlim(1, 2) +
+  labs(title = "Upregulated genes") +
+  theme_bw()
+
+ggplotly(vplotup)
+?patchwork
+design <- "AAAAAA
+AAAAAA
+BBBCCC"
+
+vplot + vplotdown + vplotup +
+  plot_layout(design = design) +
+  plot_annotation(tag_levels = "A")
+ggsave("vplotCombined.png")
