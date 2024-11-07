86ca431679ba0bab3059bc947e022ad4160726c1
ec-evanmv <evanmv@uio.no>
2024-11-07 10:01:53 +0100

    Cleaned and tested

--- 1_FeatureCounts.R
+++ 1_FeatureCounts.R
@@ -6,14 +6,14 @@ BiocManager::install("Rsubread")
 library(Rsubread) #featureCounts
 
 ##featureCounts -----
-fC <- featureCounts(files = c("alignment_output/B1_AlignedReads.sam","alignment_output/B2_AlignedReads.sam","alignment_output/B3_AlignedReads.sam", "alignment_output/C1_AlignedReads.sam","alignment_output/C2_AlignedReads.sam","alignment_output/C3_AlignedReads.sam"),
+fc <- featureCounts(files = c("alignment_output/B1_AlignedReads.sam","alignment_output/B2_AlignedReads.sam","alignment_output/B3_AlignedReads.sam", "alignment_output/C1_AlignedReads.sam","alignment_output/C2_AlignedReads.sam","alignment_output/C3_AlignedReads.sam"),
               annot.inbuilt = "hg38",
               useMetaFeatures = TRUE,
               isPairedEnd = TRUE,
               nthreads = 8)
-View(fC$annotation)
-View(fC$stat)
-View(fC$counts)
+View(fc$annotation)
+View(fc$stat)
+View(fc$counts)
 
-fC_counts["4322",] #MMP13 counts
-fC_counts["22806",]
+fc$counts["4322",] #MMP13 counts
+fc$counts["22806",]
