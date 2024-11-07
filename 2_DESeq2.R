23f6c96315f8594607c1df842777c26f556c464f
evanmv <evanmv@uio.no>
2024-11-07 12:51:11 +0100

    Cleaned up and added small multiples plot. Tested

--- 2_DESeq2.R
+++ 2_DESeq2.R
@@ -48,7 +48,7 @@ res_nf
 #Generate report with regionReport
 #Problem with regionReport?
 dir.create('DESeq2_nf_out', showWarnings = FALSE, recursive = TRUE)
-report <- DESeq2Report(dds_nf, project = 'MMP13 knockdown in DOK', intgroup = 'group', res = 'res.nf', outdir = 'DESeq2.nf_out', output = 'index')
+report <- DESeq2Report(dds_nf, project = 'MMP13 knockdown in DOK', intgroup = 'group', res = 'res_nf', outdir = 'DESeq2_nf_out', output = 'index')
 ??DESeq2Report
 
 #Shrinkage of effect size with apeglm
@@ -59,37 +59,82 @@ res_lfc <- lfcShrink(dds_nf,
 summary(res_lfc)
 
 plotMA(res_lfc, ylim = c(-2,2)) #plot log2 FCs over mean of normalized counts for all samples.
-idx <- identify(res$baseMean, res$log2FoldChange) #Id row number of individual genes. Click in plot then click finish
-rownames(res)[idx]
+idx <- identify(res_nf$baseMean, res_nf$log2FoldChange) #Id row number of individual genes. Click in plot then click finish
+rownames(res_nf)[idx] #Helpful after fetching gene names
 
 #Subset "significant" results
-resOrd.nf <- res.nf[order(res.nf$pvalue),]
-summary(resOrd.nf)
-resSig.nf <- subset(resOrd.nf, padj < 0.05) #Subset of ordered dataset with significance less that 0.05
-summary(resSig.nf)
+res_ord <- res_nf[order(res_nf$pvalue),]
+summary(res_ord)
+res_sig <- subset(res_ord, padj < 0.05) #Subset of ordered dataset with significance less that 0.05
+summary(res_sig)
 
 ##PCA -----
 #Variance stabilizing transformation
 
-vsd.nf <- vst(dds.nf)
-head(assay(vsd.nf), 3)
+vsd <- vst(dds_nf)
+head(assay(vsd), 3)
 
 #PCA 
 #plotPCA(vsd, intgroup="group") #use returnData=TRUE to save pca data to object
-pcaData.nf <- plotPCA(vsd.nf, intgroup="group", returnData=TRUE)
-percentVar.nf <- formatC(100 * attr(pcaData.nf, "percentVar"))
-
-#plot w/ GGplot
-PCA_nf <- ggplot(pcaData.nf, aes(PC1, PC2, color=group)) +
+pca_data <- plotPCA(vsd, intgroup="group", returnData=TRUE)
+percent_var <- formatC(100 * attr(pca_data, "percentVar"))
+?plotPCA
+#plotPCA#plot w/ GGplot
+PCA <- ggplot(pca_data, aes(PC1, PC2, color=group)) +
   geom_point(size=5) +
-  xlab(paste0("PC1 (",percentVar.nf[1],"%)")) +
-  ylab(paste0("PC2 (",percentVar.nf[2],"%)")) +
+  xlab(paste0("PC1 (",percent_var[1],"%)")) +
+  ylab(paste0("PC2 (",percent_var[2],"%)")) +
   coord_fixed() +
   theme_bw()
 
-ggsave("PCA_nf.png")
+ggsave("PCA.png")
 
 #Small multiples plot
 
+# Create a PCA 'small multiples' chart ----
+# this is another way to view PCA laodings to understand impact of each sample on each pricipal component
+#Uses top 500 variable genes, same as plotPCA from DESeq2
+ntop <- 500 
+rv <- rowVars(assay(vsd), useNames = T)
+select <- order(rv, decreasing = TRUE)[seq_len(min(ntop, length(rv)))]
+mat <- t(assay(vsd)[select, ] )
+
+pca_prcomp <- prcomp(mat)
+pca <- as.data.frame(pca_prcomp$x)
+pca$PC1 #Check that PC1 matches plotPCA
+pca_data$PC1
+?prcomp
+
+#To get % variance
+summ <- summary(pca_prcomp)
+percent_var_prcomp <- formatC(100 * summ$importance[2, ])
+
+#Top 4 principal components
+pca_res <- pca[,1:4] %>% 
+  as_tibble() %>%
+  add_column(sample = rownames(col_data),
+             group = col_data$group) 
+#Add percent variance to colnames
+names(pca_res)[names(pca_res) == 'PC1'] <- paste0("PC1 (",percent_var_prcomp[1],"%)")
+names(pca_res)[names(pca_res) == 'PC2'] <- paste0("PC2 (",percent_var_prcomp[2],"%)")
+names(pca_res)[names(pca_res) == 'PC3'] <- paste0("PC3 (",percent_var_prcomp[3],"%)")
+names(pca_res)[names(pca_res) == 'PC4'] <- paste0("PC4 (",percent_var_prcomp[4],"%)")
 
 
+pca_pivot <- pivot_longer(pca_res, # dataframe to be pivoted
+                          cols = 1:4, # column names to be stored as a SINGLE variable
+                          names_to = "PC", # name of that new variable (column)
+                          values_to = "loadings") # name of new variable (column) storing all the values (data)
+??Magrittr
+ggplot(pca_pivot) +
+  aes(x=sample, y=loadings, fill=group) + # you could iteratively 'paint' different covariates onto this plot using the 'fill' aes
+  geom_bar(stat="identity") +
+  facet_wrap(~PC) +
+  labs(title="PCA 'small multiples' plot") +
+  theme_bw() +
+  coord_flip()
+
+ggsave('smallMultiples.png')
+?geom_bar
+?labs
+?facet_wrap
